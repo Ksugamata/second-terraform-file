1045972f74c3fc4473a5407fafd5ed0601cb27dc
Ksugamata <ksugamata@bltinc.co.jp>
2025-05-27 10:20:31 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "zone_id" {}
 
 variable "domain_name" {
-  description = "asuranox.jp"
+  description = "example.com"
   type        = string
 }
 
 variable "acm_cert_arn" {
-  description = "arn:aws:acm:us-east-1:205144096702:certificate/9f6474c5-dbc2-429d-b007-a35521a057f6"
+  description = "arn:aws:acm:us-east-1:xxxxxxxxxxx:certificate/xxxxxxxxxxx-xxxxxxxxx"
   type        = string
 }
